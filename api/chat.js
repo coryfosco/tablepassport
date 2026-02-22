e69a4f2a7121c8be2a1f47025033523ada0d6ae0
coryfosco <coryfosco@gmail.com>
2026-02-22 11:41:57 -0600

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-api-key': 'PASTE_YOUR_API_KEY_HERE',
+        'x-api-key': process.env.ANTHROPIC_API_KEY,
         'anthropic-version': '2023-06-01'
       },
       body: JSON.stringify(req.body)
